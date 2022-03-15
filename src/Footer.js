8ace327865870d390b4b5398a9f7cc0759cb1711
PatPetChav <paelpec@yahoo.com>
2022-03-15 01:09:54 -0300

    react-inicio

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div>
+        <footer>
+            <h3>All Copyright @ reserved</h3>
+        </footer>
+
+    </div>  
+
+    )
+    
+
+}
+
+export default Footer;
